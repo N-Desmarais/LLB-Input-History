b84057e2b895ca60c21b2c6497ba59510d0d61c5
N-Desmarais <55117352+N-Desmarais@users.noreply.github.com>
2020-10-05 23:21:57 -0400

    Initial Commit
    Basic mod setup and test.

---
+++ LLBInputHistory/LLBInputHistory.cs
@@ -0,0 +1,21 @@
+﻿using Rewired.Internal;
+using System;
+using UnityEngine;
+
+namespace LLBInputHistory
+{
+    public class LLBInputHistory : MonoBehaviour
+    {
+        public static LLBInputHistory instance = null;
+
+        public static void Initialize() {
+            GameObject gameObject = new GameObject("LLBInputHistory");
+            instance = gameObject.AddComponent<LLBInputHistory>();
+            DontDestroyOnLoad(gameObject);   
+        }
+        private void OnGUI() {
+            GUI.contentColor = Color.red;
+            GUI.Label(new Rect(10, 10, 100, 25), "LLBM Mod Test");
+        }
+    }
+}
