37d7426bea907f1f23d5df23b23fd25c166c1ab5
N-Desmarais <55117352+N-Desmarais@users.noreply.github.com>
2020-12-11 02:05:07 -0500

    Asset Ripping Time
    Attempting to rip the textures from Daioutzu's input viewer mod

---
+++ LLBInputHistory/Assets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace LLBInputHistory {
+    public static class Assets {
+        //Asset bundle stolen from Daioutzu's input viewer mod
+        static string bundle = "C:\\Users\\njdes\\Documents\\GitHub\\LLB-Input-History\\LLBInputHistory\\Assets\\Bundles\\ui";
+
+        static AssetBundle stolenBundle;
+        static Font inputViewerFont;
+        //public static Dictionary<string, Texture2D> stolenAssets = new Dictionary<string, Texture2D>();
+        public static Texture2D[] textures;
+
+        public static int textureNumber = 0;
+
+        public static void LoadAssets() {
+            Debug.Log("bbb");
+            try {
+            stolenBundle = AssetBundle.LoadFromFile(bundle);
+            textures = stolenBundle.LoadAllAssets<Texture2D>();
+            foreach (Texture2D t in textures) {
+                if (t.name != "Font Texture") {
+                    byte[] bytes = t.EncodeToPNG();
+                    File.WriteAllBytes("C:\\Users\\njdes\\Documents\\GitHub\\LLB-Input-History\\LLBInputHistory\\Assets\\" + t.name + ".png", bytes);
+                }
+            }
+            inputViewerFont = stolenBundle.LoadAsset<Font>("assets/ui/elements.ttf");
+            } catch (Exception e) {
+                Debug.Log(e.ToString());
+            }
+        }
+
+        public static Texture2D StolenAssetViewer() {
+            Texture2D texture = textures[textureNumber];
+            return texture;
+        }
+    }
+}
--- LLBInputHistory/LLBInputHistory.cs
+++ LLBInputHistory/LLBInputHistory.cs
@@ -15,7 +15,8 @@ namespace LLBInputHistory
         public static void Initialize() {
             GameObject gameObject = new GameObject("LLBInputHistory");
             instance = gameObject.AddComponent<LLBInputHistory>();
-            DontDestroyOnLoad(gameObject);   
+            DontDestroyOnLoad(gameObject);
+            Assets.LoadAssets();
         }
 
         public ModMenuIntegration MMI = null;
@@ -24,10 +25,23 @@ namespace LLBInputHistory
             if (MMI == null) { MMI = gameObject.AddComponent<ModMenuIntegration>(); }
         }
 
-
+        public const float GUI_Height = 1080;
+        public const float GUI_Width = 1920;
+        Rect inputRect = new Rect(30, GUI_Height - 147, 300, 117);
         private void OnGUI() {
-            GUI.contentColor = Color.red;
-            GUI.Label(new Rect(10, 10, 100, 25), "LLBM Mod Test");
+            if(LLModMenu.ModMenu.Instance.inModOptions) inputRect = GUILayout.Window(102289, inputRect, guiFunc,"asset");
+
+        }
+
+        void guiFunc(int wId) {
+            GUILayout.BeginVertical();
+            GUILayout.Box(Assets.StolenAssetViewer());
+            if (GUILayout.Button("next texture")) {
+                Assets.textureNumber++;
+                if (Assets.textureNumber == Assets.textures.Length) Assets.textureNumber = 0;
+            }
+            GUILayout.EndVertical();
+            GUI.DragWindow(new Rect(0, 0, 500, 500));
         }
     }
 }
